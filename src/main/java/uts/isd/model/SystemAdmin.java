faeadb417fb815425d3146cbba6e3367fca910bb
Ifhaam Zuhree <mzmifhaam@gmail.com>
2024-04-14 12:43:57 +1000

    Update SystemAdmin.java

--- src/main/java/uts/isd/model/SystemAdmin.java
+++ src/main/java/uts/isd/model/SystemAdmin.java
@@ -3,9 +3,31 @@ package uts.isd.model;
 import java.util.*;
 public class SystemAdmin extends User {
     
+    private int adminID;
+    private String accessPrivileges;
     
-    public SystemAdmin(String _email, String _name, String _password, String _phone, String _city, String _country, String _role)
-    {
+    public SystemAdmin(String _email, String _name, String _password, String _phone, String _city, String _country,
+            String _role, int adminID, String accessPrivileges) {
         super(_email, _name, _password, _phone, _city, _country, _role);
+        this.adminID = adminID;
+        this.accessPrivileges = accessPrivileges;
     }
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getAccessPrivileges() {
+        return accessPrivileges;
+    }
+
+    public void setAccessPrivileges(String accessPrivileges) {
+        this.accessPrivileges = accessPrivileges;
+    }
+
+    
 }
