e04668b382e7e9fb06cee51f550d30063133dfee
Ifhaam Zuhree <mzmifhaam@gmail.com>
2024-04-14 12:42:30 +1000

    Update Staff.java

--- src/main/java/uts/isd/model/Staff.java
+++ src/main/java/uts/isd/model/Staff.java
@@ -2,10 +2,43 @@ package uts.isd.model;
 
 import java.util.*;
 
-public class Staff extends User{
-
-    public Staff(String _email, String _name, String _password, String _phone, String _city, String _country, String _role)
-    {
+public class Staff extends User {
+    
+    private int staffID;
+    private String role;
+    private int accessLevel;
+    
+    public Staff(String _email, String _name, String _password, String _phone, String _city, String _country,
+            String _role, int staffID, String role, int accessLevel) {
         super(_email, _name, _password, _phone, _city, _country, _role);
+        this.staffID = staffID;
+        this.role = role;
+        this.accessLevel = accessLevel;
     }
+
+    public int getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(int staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    
 }
